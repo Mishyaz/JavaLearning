c3652feeed9bd5d1c66187eb459f7260e9c9a4b7
Mishyaz <123qweASD>
2016-12-08 12:17:18 +0200

    Second change - new format of "sot"

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,7 @@
 package Main;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello!");
+    public static void main(String[] args) {
+        System.out.format(" Hello %s !", args[0]);
     }
 }
